744204ed6bc7325141587e5a687c3ae9e2c958e2
TwilightLogic <twilightlogic@gmail.com>
2024-06-28 16:37:49 +0800

    feat

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,13 @@
+import { useRouter } from 'next/router'
+
 export default {
   footer: <p>MIT 2023 © Nextra.</p>,
   head: ({ title, meta }) => {
-    console.log(title)
-    console.log(meta)
+    const router = useRouter()
+    const isPhotosPage = router.pathname.includes('photos')
+
+    console.log(isPhotosPage)
+
     return (
       <>
         {meta.description && (
@@ -10,10 +15,19 @@ export default {
         )}
         {meta.tag && <meta name="keywords" content={meta.tag} />}
         {meta.author && <meta name="author" content={meta.author} />}
+        {isPhotosPage && meta.image && (
+          <meta name="twitter:image" content={meta.image} />
+        )}
       </>
     )
   },
   readMore: 'Read More →',
   postFooter: null,
-  darkMode: false
+  darkMode: false,
+  navs: [
+    {
+      url: 'https://github.com/shuding/nextra',
+      name: 'Nextra'
+    }
+  ]
 }
